e4cc6459f166fbde7d2ab6aebc11517db75e2dee
roccas98 <simonerocca18@gmail.com>
2024-05-14 15:10:55 +0200

    prova db

--- Sentiment_Setter/types/generated/components.d.ts
+++ Sentiment_Setter/types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface TableTabella1 extends Schema.Component {
+  collectionName: 'components_table_tabella1s';
+  info: {
+    displayName: 'tabella1';
+  };
+  attributes: {
+    nome: Attribute.String;
+    frase: Attribute.String;
+    flag: Attribute.Boolean;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'table.tabella1': TableTabella1;
+    }
+  }
 }

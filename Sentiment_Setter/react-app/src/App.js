7dbf37a37d30d0c1df23bee16ddb75b1b37ef267
roccas98 <simonerocca18@gmail.com>
2024-06-19 09:12:00 +0200

    update import

--- Sentiment_Setter/react-app/src/App.js
+++ Sentiment_Setter/react-app/src/App.js
@@ -1,31 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 import { BrowserRouter, Route,Routes ,Switch} from "react-router-dom";
 import DashboardPage from "./components/DashboardPage";
-import { Container } from "reactstrap";
+
 import Login from './components/Login';
 import Registration from './components/Register';
 import AnnotationPage from "./components/AnnotationPage";
-//import Frasi_Classificate from './components/Frasi_classificate'; 
 import LandingPage from './components/LandingPage';
 import UserProfilePage from './components/UserProfilePage';
-//import RecapPage from './components/RecapPage';
+
 import AdminDashboard from './components/Admin/AdminDashboard';
 import AdminAnnotationPage from './components/AdminAnnotationPage';
-//import Logout from "./components/Logout";
-import { ToastContainer } from "react-toastify";
 import  Protector  from "./components/Protector/Protector";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-//import { SentimentProvider } from './context/SentimentContext';  // Import the SentimentContext
-//import  ClassificationProvider  from './context/ClassificationProvider';
-
 import  AuthProvider  from './context/AuthProvider';
-
-//import 'primeicons/primeicons.css';
 import { PrimeReactProvider } from 'primereact/api';
-//import 'primeflex/primeflex.css';  
+
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 
